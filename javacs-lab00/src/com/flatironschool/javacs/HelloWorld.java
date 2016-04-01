22c4a0f15731d1fecd79fba4a9a585a9a8733413
tiendt <dao23t@mtholyoke.edu>
2016-04-01 16:00:18 -0400

    Done.

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -2,10 +2,12 @@ package com.flatironschool.javacs;
 
 public class HelloWorld {
     public static Double getVersion() {
-        // I won't work!
+        String str = System.getProperty("java.specification.version");
+        return Double.parseDouble (str);
     }
 
     public static void main(String[] args) {
-	    // you can test the output of getVersion() here
+	    Double test = getVersion();
+      System.out.println(test);
     }
 }
